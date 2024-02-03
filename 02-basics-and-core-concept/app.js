34bf2fd43afeca272254286c5b29e4623e4de3c1
chany-code <channn926@gmail.com>
2024-02-03 19:37:51 +0900

    methods, data binding, v-html

--- 02-basics-and-core-concept/app.js
+++ 02-basics-and-core-concept/app.js
@@ -1,9 +1,20 @@
 const app = Vue.createApp({
     data() {    // 항상 객체를 반환. Vue가 제어하는 어느 HTML에서든 사용 가능한 값들을 return
         return {
-            courseGoal: 'I love Pomeranian',
+            courseGoalA: 'I love Pomeranian',
+            courseGoalB: '<h2>Master Vue and build amazing apps!</h2>',
             vueLink: 'https://vuejs.org/'
         }; 
+    },
+    methods: {
+        outputGoal() {
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5){
+                return this.courseGoalA;
+            } else {
+                return this.courseGoalB;
+            }
+        }
     }
 });
 
